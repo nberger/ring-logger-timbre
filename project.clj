426351e889161fb2e6bfcce7359f860b6fc9daf1
Jay Sherby <JayAndCatchFire@users.noreply.github.com>
2017-06-29 14:28:25 -0500

    Bumped all dependency versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring-logger "0.7.5"]
-                 [com.taoensso/timbre "4.1.1"]]
-  :profiles {:dev {:dependencies [[ring/ring-mock "0.2.0"]
-                                  [ring/ring-codec "1.0.0"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring-logger "0.7.7"]
+                 [com.taoensso/timbre "4.10.0"]]
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]
+                                  [ring/ring-codec "1.0.1"]]}})
